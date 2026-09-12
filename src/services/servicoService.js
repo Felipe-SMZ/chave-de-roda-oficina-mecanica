6f26e5c8e49745f96fee7f7e34058e6533996e5f
Felipe <felipeshimizu5@gmail.com>
2026-09-12 20:51:55 -0300

    chore: serparando o firebaseConfig para uma camada de config

--- src/services/servicoService.js
+++ src/services/servicoService.js
@@ -1,5 +1,5 @@
 import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from "firebase/firestore";
-import { db } from "./firebaseConfig";
+import { db } from "../config/firebaseConfig";
 
 export async function carregarServicos() {
     const snapshot = await getDocs(collection(db, "servicos"));
