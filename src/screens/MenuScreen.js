64bf84858823e2805d3aa42623ad7051da00ad47
Felipe <felipeshimizu5@gmail.com>
2026-09-12 01:55:49 -0300

    feat: criando o MenuScreen

---
+++ src/screens/MenuScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, Button } from "react-native";
+
+export default function MenuScreen({ navigation }) {
+    return (
+        <View style={{ padding: 20, gap: 12 }}>
+            <Text style={{ fontSize: 22, fontWeight: "bold" }}>Oficina Mecânica</Text>
+            <Button title="Clientes" onPress={() => navigation.navigate("ClienteList")} />
+            {/* repita para Veículos, Funcionários, Serviços, Ordens de Serviço */}
+        </View>
+    );
+}
