8d0f13d209409d46d8beea1ede05987aca9a3ee8
Felipe <felipeshimizu5@gmail.com>
2026-09-12 16:26:06 -0300

    feat: criando o service para Funcionario

---
+++ src/services/funcionarioService.js
@@ -0,0 +1,19 @@
+import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from "firebase/firestore";
+import { db } from "./firebaseConfig";
+
+export async function carregarFuncionarios() {
+    const snapshot = await getDocs(collection(db, "funcionarios"));
+    return snapshot.docs.map((d) => ({ id: d.id, ...d.data() }));
+}
+
+export async function criarFuncionario(dados) {
+    return addDoc(collection(db, "funcionarios"), dados);
+}
+
+export async function atualizarFuncionario(id, dados) {
+    return updateDoc(doc(db, "funcionarios", id), dados);
+}
+
+export async function excluirFuncionario(id) {
+    return deleteDoc(doc(db, "funcionarios", id));
+}
